25cb799825b706c270d720af146c59e32bc6787a
Jovan <geofanvandicca@gmail.com>
2020-09-03 19:11:44 +0700

    build

---
+++ build/precache-manifest.23d7f0c5d132cfb4c6d58c2f5e7fc07d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "441491034568c268dcf12da6f43eecc1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b6200713339a1f08322",
+    "url": "/static/css/main.7ef0af2b.chunk.css"
+  },
+  {
+    "revision": "c9b8c7bc27b5a5376b56",
+    "url": "/static/js/2.b2fa2669.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b2fa2669.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b6200713339a1f08322",
+    "url": "/static/js/main.aec181b2.chunk.js"
+  },
+  {
+    "revision": "a1cc40938fd411f689c8",
+    "url": "/static/js/runtime-main.f87b52a6.js"
+  }
+]);
